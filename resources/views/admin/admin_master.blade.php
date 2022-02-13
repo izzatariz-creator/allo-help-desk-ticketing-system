e2ca3949b2b163b75b2c13b7be35672efc0a76bc
Izzat Ariz <izzatariz.2000@gmail.com>
2022-02-13 08:39:49 +0800

    Commit 1.2 - Integrated Dashboard Template

---
+++ resources/views/admin/admin_master.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{ asset('backend/images/favicon.ico') }}">
+
+    <title>Sunny Admin - Dashboard</title>
+
+    <!-- Vendors Style-->
+    <link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
+
+    <!-- Style-->
+    <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">
+
+</head>
+
+<body class="hold-transition light-skin sidebar-mini theme-primary fixed">
+
+    <div class="wrapper">
+
+        <!-- Header -->
+        @include('admin.body.header')
+        <!-- /.Header -->
+
+        <!-- Sidebar -->
+        @include('admin.body.sidebar')
+        <!-- /.Sidebar -->
+
+        <!-- Main Content -->
+        @yield('admin')
+        <!-- /.Main Content -->
+
+        <!-- Footer -->
+        @include('admin.body.footer')
+        <!-- Footer -->
+
+        <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+        <div class="control-sidebar-bg"></div>
+
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- Vendor JS -->
+    <script src="{{ asset('backend/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('../assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>
+    <script src="{{ asset('../assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
+    <script src="{{ asset('../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
+
+    <!-- Sunny Admin App -->
+    <script src="{{ asset('backend/js/template.js') }}"></script>
+    <script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
+
+
+</body>
+
+</html>

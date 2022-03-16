2ca57c8088fb12a8e8991426a9996a2fceb6b76d
Izzat Ariz <izzatariz.2000@gmail.com>
2022-03-17 01:34:55 +0800

    Commit 9.7 Designed Main Page

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -61,6 +61,8 @@
     <!-- Sunny Admin App -->
     <script src="{{ asset('backend/js/template.js') }}"></script>
     <script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
+    <script src="{{ asset('backend/js/pages/widget-charts2.js') }}"></script>
+    <script src="{{asset('../assets/vendor_components/chart.js-master/Chart.min.js')}}"></script>
 
     <!-- Toaster JS & Script -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
